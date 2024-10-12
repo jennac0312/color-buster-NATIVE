aef1103635a613383c0f59c51e8c66436b966181
Jenna C <jennac0312@gmail.com>
2024-10-11 20:37:32 -0400

    play again button to loop back to home screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,18 @@
-import { Text, View, SafeAreaView} from 'react-native'
+import { Text, View, SafeAreaView, Button} from 'react-native'
 
-export default function GameOverScreen() {
+export default function GameOverScreen({ navigation }) {
+
+  const handleResetGame = () => {
+    navigation.reset({
+      index: 0,
+      routes: [{name: 'Home'}]
+    })
+  }
+
     return (
       <SafeAreaView>
         <Text> GAME OVER </Text>
+        <Button title="Play Again" onPress={handleResetGame}/>
       </SafeAreaView>
     )
 }
