8c18fe9cfeb2073d8d2638c1d0e86a63da0d7814
Jenna C <jennac0312@gmail.com>
2024-10-12 13:13:29 -0400

    score update

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,10 @@
 import { useContext, useEffect, useState } from 'react'
-import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, Vibration, BackHandler } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet, TouchableOpacity, Vibration } from 'react-native'
 import { AppContext } from '../context/app_context'
 
 export default function GameScreen ({ navigation }) {
 
-    const { mode, allQuestions, GAME_LENGTH, ARCADE_ROUNDS, getColors } = useContext(AppContext)
+    const { mode, allQuestions, GAME_LENGTH, ARCADE_ROUNDS, getColors, score, setScore } = useContext(AppContext)
 
     const [ round, setRound ] = useState(0) //will need reset
     const [ currentQuestion, setCurrentQuestion ] = useState(null)
@@ -20,14 +20,16 @@ export default function GameScreen ({ navigation }) {
             setRound(cur => cur + 1)
             console.log('enter round:', round+2)
         } else {
+            setFeedbackColor('rgba(100, 131, 129, .5)')
             endGame()
         }
     }
 
     const checkAnswer = (answer, selected) => {
         if(answer === selected){
-            console.log('CORRECT')
             setFeedbackColor('rgba(50, 205, 50, .5)')
+            setScore((prev) => prev + 1) // increment score
+            console.log('CORRECT ', score)
         } else {
             console.log('INCORRECT... BUZZZZZ')
             setFeedbackColor('rgba(255, 0, 0, .5)')
@@ -35,7 +37,7 @@ export default function GameScreen ({ navigation }) {
         }
 
         setTimeout(() => {
-            setFeedbackColor('transparent')
+            setFeedbackColor('rgba(100, 131, 129, .5)')
             startNextRound()
         }, 200) // set feedback color back to transparent after ms
 
